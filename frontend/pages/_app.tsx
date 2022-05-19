223aaf0e2be26f90bf100839d9f77f3afaf6e7ec
HankTech <HankTech@gmail.com>
2022-05-19 13:09:58 -0400

    create layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/modern-normalize.min.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+//  components
+import Layout from '../components/Layout'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
